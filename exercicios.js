dee65072bfdfed647a7adcf83d89eb11bcabcf4a
Lucas Nascimento <117601245+DevLucasNascimento@users.noreply.github.com>
2023-03-15 14:18:00 -0300

    exercicio FizzBuzz

--- exercicios.js
+++ exercicios.js
@@ -1,1 +1,13 @@
+const resultado = fizzBuzz();
+console.log(resultado);
 
+function fizzBuzz(entrada){
+    if ( typeof entrada !== 'number' )
+        return 'Não é um número';
+    if ( typeof entrada % 3 === 0 && entrada % 5 === 0 )
+        return 'FizzBuzz';    
+    if ( typeof entrada % 3 === 0 )
+        return 'Fizz';
+    if ( typeof entrada % 5 === 0 )
+        return 'Buzz';
+}
